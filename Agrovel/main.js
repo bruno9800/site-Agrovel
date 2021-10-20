bfd02789ea85fc35dcd7cad8bfb03888ab4c79c1
Bruno Matheus Pereira Silva <bruno9800m@gmail.com>
2021-10-20 20:00:45 -0300

    Menu Criado e continuando o código

---
+++ Agrovel/main.js
@@ -0,0 +1,34 @@
+/* Constante para abrir e fechar o menu */
+const nav = document.querySelector('#header nav')
+const toggle = document.querySelectorAll('nav .toggle')
+
+for(const element of toggle) {
+    element.addEventListener('click', function () {
+        nav.classList.toggle('show')
+        /* se tiver show tira se não tiver coloca => Toggle == trocar */
+    })
+}
+
+/** Quando clicar em um item do menu, fechar e correr para o local */
+const links = document.querySelectorAll('nav ul li a')
+
+for(const link of links) {
+    link.addEventListener('click', function(){
+        nav.classList.remove('show')
+    })
+}
+
+/** Quando o usuario scrollar a pagina */
+
+const header = document.querySelector('#header')
+const navHeight = header.offsetHeight
+
+window.addEventListener('scroll', function() {
+    if(window.scrollY >= navHeight) {
+        // Scroll é maior que a altura do header
+        header.classList.add('scroll')
+    }else {
+        // Scroll é menor que a altura do header
+        header.classList.remove('scroll')
+    }
+})
